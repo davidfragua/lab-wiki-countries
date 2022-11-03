7fd379aae4cc5c5e453264f31e6643192d5d0336
David Fragua <davidfragua@gmail.com>
2022-11-03 22:00:18 +0100

    trying to resolve Country Details

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -7,22 +7,23 @@ function CountryDetails(props) {
 
     console.log("props", props)
     // 1. creamos un estado para los detalles del país
-    const [ countryInfo, setCountryInfo ] = useState(null)
-    // 2. buscamos el nombre del país 
+    // const [ countryInfo, setCountryInfo ] = useState(null)
+    // // 2. buscamos el nombre del país 
     const { code  } = useParams()
     // 3. contactar con la BD
-    // useEffect(() => {
-
-    //     props.map((eachCountry) =>{
-    //         return eachCountry 
-    //     })
-
-
-    // }, [code])
+    const oneCountry = props.countries.filter((eachCountry)=> {
+    return  eachCountry.alpha3Code === code
+    })
+    console.log("oneCountry", oneCountry) // veo que devuelve un array del objeto 
 
   return (
-    <div>CountryDetails
-      
+    <div>Country Details
+        {/* intento acceder a las propiedades del objeto */}
+        {/* {oneCountry.map((eachCountry)=> {
+          return  <p>{eachCountry[0].alpha3Code}</p>
+              
+        })} */}
+       <p>prueba</p>
 
         
     </div>
